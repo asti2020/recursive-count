a87e7af707cfe0dc6509d165014a61c5910b55c5
Astika Luintel <astild2020@gmail.com>
2022-12-18 19:38:30 -0500

    the count

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,10 @@
 function recursiveCount(num = 0) {
   // type your code here
+  while (num < 10) {
+    console.log(num);
+    num++;
+    recursiveCount(num);
+  }
 }
 
 if (require.main === module) {
